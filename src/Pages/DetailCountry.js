c45fa2cfc8a0cebbce4a5b7ba3a9eb002e931246
saniag081 <sgonzalezhernandez081@gmail.com>
2020-11-27 22:12:39 -0500

    add fetch detail country

--- src/Pages/DetailCountry.js
+++ src/Pages/DetailCountry.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DarkMode from '../components/DarkMode'
 import styled from 'styled-components'
 import Wrapper from '../components/Wrapper'
 import { useSelector } from 'react-redux'
+import CountrySelect from '../components/CountrySelect'
 
 const DetailCountryStyled = styled.div`
 `
 
-function DetailCountry({match}) {
-	const country = useSelector(
+function DetailCountry({ match }) {
+	const storageCountry = useSelector(
 		state => {
 			return state.countryList.find(item => item.name === match.params.name.replace('-',' '))
 		}
 	)
-	console.log(country)
+	const [getCountry, setCountry] = useState(storageCountry)
+	useEffect(() => {
+		if (!getCountry) {
+			fetch(`https://restcountries.eu/rest/v2/name/${match.params.name}`)
+				.then(response => response.json())
+				.then(data => setCountry(data[0]))
+				.catch(error => console.error(error))
+		}
+		console.log(getCountry)
+	}, [getCountry, match.params.name])
 	return (
 		<DetailCountryStyled>
 			<DarkMode/>
 			<Wrapper>
-				<p>{match.params.name}</p>
+				<CountrySelect {...getCountry} />
 			</Wrapper>
 		</DetailCountryStyled>
 	)
---
+++ src/components/CountrySelect.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CountrySelectSrtled = styled.div``
+
+function CountrySelect() {
+	return (
+		<CountrySelectSrtled>
+
+		</CountrySelectSrtled>
+	)
+}
+
+export default CountrySelect
