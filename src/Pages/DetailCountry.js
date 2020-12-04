0b7c86a416c4897ee961f7a697d7d73e3208fc92
saniag081 <sgonzalezhernandez081@gmail.com>
2020-12-03 22:35:22 -0500

    add btn back

--- src/Pages/DetailCountry.js
+++ src/Pages/DetailCountry.js
@@ -4,8 +4,23 @@ import styled from 'styled-components'
 import Wrapper from '../components/Wrapper'
 import { useSelector } from 'react-redux'
 import CountrySelect from '../components/CountrySelect'
+import { Link } from 'react-router-dom'
 
 const DetailCountryStyled = styled.div`
+	.btn{
+		max-width: 100px;
+		margin: 1rem 0;
+	}
+	.btn-back{
+		background: var(--white);
+		box-shadow: 0 0 5px rgba(0,0,0,.3);
+		padding: .5em 2.2em;
+		border-radius: 5px;
+		display: flex;
+    justify-content: center;
+		align-items: center;
+		cursor: pointer;
+	}
 `
 
 function DetailCountry({ match }) {
@@ -28,6 +43,11 @@ function DetailCountry({ match }) {
 		<DetailCountryStyled>
 			<DarkMode/>
 			<Wrapper>
+				<div className='btn'>
+					<Link to='/' className="btn-back">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="30px" height="30px"><path d="M0 0h24v24H0z" fill="none"/><path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z"/></svg>
+					</Link>
+				</div>
 				<CountrySelect {...getCountry} />
 			</Wrapper>
 		</DetailCountryStyled>
--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const CountrySelectSrtled = styled.div``
+const CountrySelectSrtled = styled.div`
+`
 
 function CountrySelect({
 	flag,
