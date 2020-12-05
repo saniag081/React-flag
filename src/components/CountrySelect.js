71afad5343d440ea4f8c29e628e3502ae26d8d15
saniag081 <sgonzalezhernandez081@gmail.com>
2020-12-04 22:02:26 -0500

    add maqueta mobile

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -2,6 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CountrySelectSrtled = styled.div`
+	display: grid;
+	padding-bottom: 3em;
+	.c-country{
+		&-img{
+			img{
+				width: 100%;
+			}
+		}
+		&-content{
+		display grid;
+		}
+	}
+	.borders{
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		p{
+			width: 100%;
+		}
+		&-item{
+			padding: .5em 2em;
+			border-radius: 5px;
+			box-shadow: 0 0 5px rgba(0,0,0,.3);
+			display: inline-flex;
+			margin-right: 10px;
+			margin-bottom: 15px;
+		}
+	}
 `
 
 function CountrySelect({
@@ -19,18 +47,27 @@ function CountrySelect({
 }) {
 	return (
 		<CountrySelectSrtled>
-			<img src={flag} alt='' />
-			<div>
-				<h2>{name}</h2>
-				<p> <strong>Native name:</strong> {nativeName}</p>
-				<p> <strong>Population:</strong> {population}</p>
-				<p> <strong>Region:</strong> {region}</p>
-				<p> <strong>Sub region:</strong> {subregion}</p>
-				<p> <strong>Capital:</strong> {capital}</p>
-				<p> <strong>Top level doaim:</strong> {topLevelDomain}</p>
-				<p> <strong>Currencies:</strong> {currencies?.map((item) => item.name)}</p>
-				<p> <strong>Languages:</strong> {languages?.map((item) => item.name)} </p>
-				<p> <strong>Borders:</strong> {borders?.map((item) => item)} </p>
+			<figure className='c-country-img'>
+				<img src={flag} alt='' />
+			</figure>
+			<h2>{name}</h2>
+			<div className='c-country-content'>
+				<div>
+					<p> <strong>Native name:</strong> {nativeName}</p>
+					<p> <strong>Population:</strong> {population}</p>
+					<p> <strong>Region:</strong> {region}</p>
+					<p> <strong>Sub region:</strong> {subregion}</p>
+					<p> <strong>Capital:</strong> {capital}</p>
+				</div>
+				<div>
+					<p> <strong>Top level doaim:</strong> {topLevelDomain}</p>
+					<p> <strong>Currencies:</strong> {currencies?.map((item) => item.name)}</p>
+					<p> <strong>Languages:</strong> {languages?.map((item) => item.name)} </p>
+				</div>
+				<div className='borders'>
+					<p> <strong>Borders:</strong> Countries: </p>
+					{borders?.map((item) => <span className='borders-item'>{item}</span>)}
+				</div>
 			</div>
 		</CountrySelectSrtled>
 	)
--- src/components/FilterByRegion.js
+++ src/components/FilterByRegion.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useSelector ,useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { getCountryListRegion } from '../actions/index';
 
 const FilterStyled = styled.select`
